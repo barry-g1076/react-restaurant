93cd5a4281e6c95cac161f136828f197079b6800
Barrrington Patterson <barry545young@gmail.com>
2023-03-06 19:46:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from './components/Header';
+import About from './components/About';
+import NavIcon from './components/NavIcon';
+import Home from './components/Home';
+import Recipe from './components/Recipe';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-poppins bg-back">
+      <Header/>
+      <NavIcon/>
+      <Home/>
+      <About/>
+      <Recipe/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
